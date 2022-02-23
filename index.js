8784655f03dee67fd11ff567665356a75938a4b6
stephenlambertfs <97977357+stephenlambertfs@users.noreply.github.com>
2022-02-23 15:28:39 -0700

    commit tree 6a5b979c04f064e26668c5116bfc94fd75ba4d92

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function saturdayFun(task){
+    if(typeof task == "undefined"){
+        return "roller-skate";
+    }
+    else{
+        "This Saturday, I want to " + task + "!";
+    }
+}
